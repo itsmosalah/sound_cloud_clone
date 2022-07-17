ea7be17fc78f0984d97657a39a0f159f29e347b6
Mohamed Salah <mosalah3480@gmail.com>
2022-07-18 01:44:30 +0200

    added firebase and fixed version errors

---
+++ lib/models/track_data.dart
@@ -0,0 +1,8 @@
+class TrackData {
+  String id, name, previewURL;
+
+  //url to images with different resolutions
+  String? image640URL, image300URL, image64URL, albumName;
+
+  TrackData({required this.id,required this.name,required this.previewURL});
+}
