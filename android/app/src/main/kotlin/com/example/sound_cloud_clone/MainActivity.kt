28ae2757e9d3136811e636ce29e76eb3d3b6f249
Mohamed Salah <mosalah3480@gmail.com>
2022-07-18 18:10:08 +0200

    deleted everything

--- android/app/src/main/kotlin/com/example/sound_cloud_clone/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sound_cloud_clone
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/models/track_data.dart
+++
@@ -1,8 +0,0 @@
-class TrackData {
-  String id, name, previewURL;
-
-  //url to images with different resolutions
-  String? image640URL, image300URL, image64URL, albumName;
-
-  TrackData({required this.id,required this.name,required this.previewURL});
-}
